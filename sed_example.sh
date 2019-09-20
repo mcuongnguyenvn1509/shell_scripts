05ef9698cd99a2621aa414b1b23e7ed9ad1f9b1c
mcuongnguyenvn1509 <44352679+mcuongnguyenvn1509@users.noreply.github.com>
2019-09-20 16:21:22 +0700

    Create sed_example.sh

---
+++ sed_example.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+ #find . -type f | xargs sed -i 's/tVoid/void/g;'
+
+ replacements=(
+     tVoid:void
+     sentence:word
+     will:need to
+     converted:repalced
+     to:with
+     words:alphabets
+ )
+
+ for row in "${replacements[@]}"; do
+     original="$(echo $row | cut -d: -f1)";
+     new="$(echo $row | cut -d: -f2)";
+     sed -i -e "s/${original}/${new}/g" data.txt;
+ done
